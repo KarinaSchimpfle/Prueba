2ffce9910e69590b9ed097f3368b791168961dac
DESKTOP-1NR5180\gero <geroleleant@gmail.com>
2018-08-27 17:58:07 -0300

    ehh aca la idea seria que solo quede un System.out.println pero bueno

--- holaMundo.java
+++ holaMundo.java
@@ -2,8 +2,9 @@
 public class holaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("hola mundo/hi wo");
-
+		System.out.println("hola mundo/hi wo prueba 1");
+		//prueba 2
+		int i=0;
 	}
 
 }
